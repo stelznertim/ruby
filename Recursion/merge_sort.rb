021075668f1494735f63120fd13d515421517d68
Tim Stelzner <tim.stelzner@ivx.com>
2021-11-19 14:32:15 +0100

    finalized merge_sort function

---
+++ Recursion/merge_sort.rb
@@ -0,0 +1,31 @@
+def merge_sort(array)
+  if array.size < 2 
+    return array
+  else 
+    left = merge_sort(array[0...array.size / 2])
+    right = merge_sort(array[array.size / 2... array.size])
+    merge(left,right)
+  end
+end
+
+def merge ( left, right, array = [])
+  (left.size + right.size).times do
+    if left.empty?
+      array << right.shift
+    elsif right.empty?
+      array << left.shift
+    else
+      comparison = left <=> right
+      if comparison == -1
+        array << left.shift
+      elsif comparison == 1
+        array << right.shift
+      else
+        array << left.shift
+      end
+    end
+  end
+  array
+end
+
+pp merge_sort([1,5,4,7,6,9,2,10,0])
