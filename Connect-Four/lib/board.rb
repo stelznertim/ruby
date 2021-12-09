35132fcf1484811734d72020d6343200c206a00f
Tim Stelzner <tim.stelzner@ivx.com>
2021-12-09 13:56:47 +0100

    refined the tested methods

--- Connect-Four/lib/board.rb
+++ Connect-Four/lib/board.rb
@@ -5,10 +5,11 @@ class Board
     @rows = 6
     @columns = 7
     @grid = Array.new(6) { Array.new(7) }
+    @current_symbol = ''
   end
 
   def valid_column?(input)
-    return true if input < columns && check_rows(input)
+    return true if input >= 0 && input < columns && check_rows(input)
 
     puts 'The column is not a valid selection. Please select another column.'
     false
@@ -22,33 +23,36 @@ class Board
   end
 
   def update(column, symbol)
+    return 'You are not allowed to play two turns in a row' if symbol == @current_symbol
+
+    @current_symbol = symbol
     grid.reverse.each_with_index do |row, row_index|
       next unless row[column].nil?
 
-      grid.reverse[row_index][column] = symbol
+      grid[5 - row_index][column] = symbol
       break
     end
     show
     grid
   end
 
-  def game_over?(symbol)
+  def game_over?
     grid.each do |row|
-      return true if check_grid_line(row, symbol)
+      return true if check_grid_line(row, @current_symbol)
     end
 
     grid.transpose.each do |column|
-      return true if check_grid_line(column, symbol)
+      return true if check_grid_line(column, @current_symbol)
     end
 
     diagonals = find_diagonals(grid)
     diagonals.each do |diagonal|
-      return true if check_grid_line(diagonal, symbol)
+      return true if check_grid_line(diagonal, @current_symbol)
     end
 
     reverse_diagonals = find_diagonals(grid.reverse)
     reverse_diagonals.each do |reverse_diagonal|
-      return true if check_grid_line(reverse_diagonal, symbol)
+      return true if check_grid_line(reverse_diagonal, @current_symbol)
     end
     false
   end
@@ -77,15 +81,15 @@ class Board
   def show
     puts <<-BOARD
     #{grid[0][0]} | #{grid[0][1]} | #{grid[0][2]} | #{grid[0][3]} | #{grid[0][4]} | #{grid[0][5]} | #{grid[0][6]}
-    -+--+--+--+--+--+--
+    ----------------------
     #{grid[1][0]} | #{grid[1][1]} | #{grid[1][2]} | #{grid[1][3]} | #{grid[1][4]} | #{grid[1][5]} | #{grid[1][6]}
-    -+--+--+--+--+--+--
+    ----------------------
     #{grid[2][0]} | #{grid[2][1]} | #{grid[2][2]} | #{grid[2][3]} | #{grid[2][4]} | #{grid[2][5]} | #{grid[2][6]}
-    -+--+--+--+--+--+-
+    ---------------------
     #{grid[3][0]} | #{grid[3][1]} | #{grid[3][2]} | #{grid[3][3]} | #{grid[3][4]} | #{grid[3][5]} | #{grid[3][6]}
-    -+--+--+--+--+--+-
+    ---------------------
     #{grid[4][0]} | #{grid[4][1]} | #{grid[4][2]} | #{grid[4][3]} | #{grid[4][4]} | #{grid[4][5]} | #{grid[4][6]}
-    -+--+--+--+--+--+-
+    ---------------------
     #{grid[5][0]} | #{grid[5][1]} | #{grid[5][2]} | #{grid[5][3]} | #{grid[5][4]} | #{grid[5][5]} | #{grid[5][6]}
     BOARD
   end
@@ -96,3 +100,8 @@ class Board
     true
   end
 end
+
+board = Board.new
+board.update(1, 'x')
+board.update(1, 't')
+board.update(1, 'x')
--- Connect-Four/spec/board_spec.rb
+++ Connect-Four/spec/board_spec.rb
@@ -2,100 +2,128 @@ require_relative '../lib/board'
 
 describe Board do
   let(:board) { Board.new }
-  context 'the initialized attributes of board are right' do
-    it 'the number of rows is 6' do
-      expect(board.rows).to eq(6)
+  let(:column_count) { 7 }
+  let(:row_count) { 6 }
+
+  context 'the initialized attributes are according to the ruleset' do
+    it 'has a fixed number of rows' do
+      expect(board.rows).to eq(row_count)
     end
 
-    it 'the number of columns is 7' do
-      expect(board.columns).to eq(7)
+    it 'has a fix number of columns' do
+      expect(board.columns).to eq(column_count)
     end
   end
 
-  context 'the initialized attributes of board are wrong' do
-    it ' the number of rows is not 7' do
-      expect(board.rows).not_to eq(7)
-    end
-    it 'the number of columns is not 8' do
-      expect(board.columns).not_to eq(8)
-    end
-  end
-
-  context 'the column is valid' do
-    it '5 is a valid column' do
-      expect(board.valid_column?(5)).to be true
-    end
-  end
-
-  context ' the column is not valid ' do
-    it '20 is not a valid column' do
-      expect(board.valid_column?(20)).to be false
-    end
-  end
-  context ' the column is full' do
-    before do
-      column = 4
-      6.times do
-        board.update(column, '©')
+  describe '.valid_column?' do # für eigenständige methoden immer describe benutzen
+    context 'when column is empty' do
+      it ' return true for valid column numbers' do
+        expect(board.valid_column?(column_count - 1)).to be true
+        expect(board.valid_column?(0)).to be true
+      end
+      it 'return false for invalid column numbers' do
+        expect(board.valid_column?(column_count)).to be false
+        expect(board.valid_column?(-1)).to be false
       end
     end
-    it 'a full column 4 is not valid' do
-      expect(board.valid_column?(4)).not_to be true
+    context 'when column is full' do
+      let(:column) { 4 }
+      let(:row_count) { 6 }
+      it ' return false for full column' do
+        row_count.times do
+          board.update(column, '©')
+          board.update(column - 1, 'å')
+        end
+        expect(board.valid_column?(column)).to be false
+      end
     end
   end
 
-  context 'grid updates' do
-    second_board = Board.new
-    column = 4
-    max_row = 6
-    symbol = 'o'
+  describe '.update' do
+    let(:second_board) { Board.new }
+    let(:column) { 4 }
+    let(:max_row) { 6 }
+    let(:symbol)  { '†' }
     it 'updates tile in column 4' do
+      expect(second_board.grid[max_row - 1][column]).not_to eq(symbol)
       second_board.update(column, symbol)
-      expect(second_board.grid[max_row - 1][column - 1]).to eq(symbol)
+      expect(second_board.grid[max_row - 1][column]).to eq(symbol)
     end
     it 'updates tile in column 4 and row 5 because column 4 row 6 is already occupied' do
+      expect(second_board.grid[max_row - 2][column]).not_to eq(symbol)
+      second_board.update(column, symbol)
+      second_board.update(1, 'å') # turn of player 2, not of interest but needed to be able to make the 2nd move for the player
+      second_board.update(column, symbol)
+      expect(second_board.grid[max_row - 2][column]).to eq(symbol)
+    end
+    it 'returns error message when the same palyer plays 2 turns in a row' do
       second_board.update(column, symbol)
       second_board.update(column, symbol)
-      expect(second_board.grid[max_row - 2][column - 1]).to eq(symbol)
+      expect(second_board.update(column, symbol)).to eq('You are not allowed to play two turns in a row')
     end
   end
 
-  context 'game is over' do
+  describe '.game_over?' do
+    let(:symbol_one) { '†' }
+    let(:symbol_two) { 'å' }
     it 'has four vertical discs' do
       vertical_board = Board.new
-      vertical_board.update(1, '†')
-      vertical_board.update(1, '†')
-      vertical_board.update(1, '†')
-      vertical_board.update(1, '†')
-      expect(vertical_board.game_over?('†')).to be true
+      vertical_board.update(1, symbol_one)
+      vertical_board.update(2, symbol_two)
+      vertical_board.update(1, symbol_one)
+      vertical_board.update(2, symbol_two)
+      vertical_board.update(1, symbol_one)
+      vertical_board.update(2, symbol_two)
+      vertical_board.update(1, symbol_one)
+      expect(vertical_board.game_over?).to be true
     end
     it 'has four horizontal discs' do
       horizontal_board = Board.new
-      horizontal_board.update(1, '†')
-      horizontal_board.update(2, '†')
-      horizontal_board.update(3, '†')
-      horizontal_board.update(4, '†')
-      expect(horizontal_board.game_over?('†')).to be true
+      horizontal_board.update(1, symbol_one)
+      horizontal_board.update(1, symbol_two)
+      horizontal_board.update(2, symbol_one)
+      horizontal_board.update(1, symbol_two)
+      horizontal_board.update(3, symbol_one)
+      horizontal_board.update(1, symbol_two)
+      horizontal_board.update(4, symbol_one)
+      expect(horizontal_board.game_over?).to be true
     end
     it 'has four diagonal discs' do
       diagonal_board = Board.new
-      diagonal_board.update(1, '†')
-      diagonal_board.update(1, '†')
-      diagonal_board.update(1, '†')
-      diagonal_board.update(1, '†')
-      diagonal_board.update(2, '†')
-      diagonal_board.update(2, '†')
-      diagonal_board.update(2, '†')
-      diagonal_board.update(3, '†')
-      diagonal_board.update(3, '†')
-      diagonal_board.update(4, '†')
-      expect(diagonal_board.game_over?('†')).to be true
+      diagonal_board.update(1, symbol_one)
+      diagonal_board.update(1, symbol_two)
+      diagonal_board.update(1, symbol_one)
+      diagonal_board.update(5, symbol_two)
+      diagonal_board.update(1, symbol_one)
+      diagonal_board.update(5, symbol_two)
+      diagonal_board.update(2, symbol_one)
+      diagonal_board.update(2, symbol_two)
+      diagonal_board.update(2, symbol_one)
+      diagonal_board.update(3, symbol_two)
+      diagonal_board.update(3, symbol_one)
+      diagonal_board.update(5, symbol_two)
+      diagonal_board.update(4, symbol_one)
+      expect(diagonal_board.game_over?).to be true
     end
-  end
-
-  context 'game is not over' do
-    it 'fulfills no win condition' do
-      expect(board.game_over?('†')).not_to be true
+    it 'has four reversed diagonal discs' do
+      diagonal_board = Board.new
+      diagonal_board.update(1, symbol_one)
+      diagonal_board.update(1, symbol_two)
+      diagonal_board.update(2, symbol_one)
+      diagonal_board.update(1, symbol_two)
+      diagonal_board.update(2, symbol_one)
+      diagonal_board.update(3, symbol_two)
+      diagonal_board.update(3, symbol_one)
+      diagonal_board.update(1, symbol_two)
+      diagonal_board.update(3, symbol_one)
+      diagonal_board.update(4, symbol_two)
+      diagonal_board.update(4, symbol_one)
+      diagonal_board.update(4, symbol_two)
+      diagonal_board.update(4, symbol_one)
+      expect(diagonal_board.game_over?).to be true
+    end
+    it 'returns false without win condition' do
+      expect(board.game_over?).to be false
     end
   end
 end
